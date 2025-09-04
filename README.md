5e1de1a932bb97160c6f6158142397e086d72bd2
devnetkid <jessefausett@gmail.com>
2025-09-04 12:45:54 -0600

    Added basic logging fuctionality

---
+++ README.md
@@ -0,0 +1,14 @@
+# jlabs
+
+## Logging setup
+
+To setup logging you can add two environment variables or just use defaults.
+
+1. JLABS_LOG_PATH="your/desired/path" (relative to your home directory)
+2. JLABS_LOG_LEVEL="debug" (you can use upper or lower case, doesn't matter)
+
+If you don't set them, the default value for path is ~/jlabs/logs/ and for log level
+it is set to info.
+
+On Windows systems the home directory should be C:\Users\<username>
+
--- src/jlabs/cli.py
+++ src/jlabs/cli.py
@@ -1,15 +1,34 @@
-"""Usage:
-
-    jlabs
-    jlabs -h | --help | --version
-
-"""
+"""Usage: jlabs [--version] [-h | --help]"""
 
 import logging
+import os
+from datetime import datetime
+from pathlib import Path
 
 from docopt import docopt
 
+# Dynamically create the log filename using current timestamp
+logname = f"jlabs_{datetime.now():%Y-%m-%d_%H-%M-%S}.log"
+
+# Setup logging directory relative to users home directory
+log_path = Path.home() / os.getenv("JLABS_LOG_PATH", Path("jlabs/logs/"))
+log_path.mkdir(parents=True, exist_ok=True)  # Creates directory if not already there
+
+# Check environment variables for log level, defaults to info if none provided
+log_level = getattr(logging, os.getenv("JLABS_LOG_LEVEL", "info").upper(), None)
+if not isinstance(log_level, int):
+    raise ValueError("Invalid log level")
+
+# Configure logging
+logging.basicConfig(
+    filename=log_path / logname,
+    encoding="utf-8",
+    level=log_level,
+    format="%(asctime)s %(filename)14s:%(lineno)s %(levelname)11s > %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
+
 
 def run():
-    arguments = docopt(__doc__, version='0.0.0')
-    print(arguments)
+    arguments = docopt(__doc__, version="0.0.0")
+    logging.debug(f"Arguments passed in:\n{arguments}")
